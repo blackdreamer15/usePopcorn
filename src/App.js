78bdc9c11f5743f662dfd5c5c793b0c4f8dac3ef
Jessy Baki <bakijessy@gmail.com>
2023-08-19 22:47:36 +0000

    Conditionally render the tempMovieData elements

--- src/App.js
+++ src/App.js
@@ -82,25 +82,27 @@ export default function App() {
             {isOpen1 ? "+" : "-"}
           </button>
 
-          <ul className="list">
-            {tempMovieData.map(movie => (
-              <li key={movie.imdbID}>
-                <img
-                  src={movie.Poster}
-                  alt={`${movie.Title} poster`}
-                />
+          {isOpen1 &&
+            <ul className="list">
+              {tempMovieData.map(movie => (
+                <li key={movie.imdbID}>
+                  <img
+                    src={movie.Poster}
+                    alt={`${movie.Title} poster`}
+                  />
 
-                <h3>{movie.Title}</h3>
+                  <h3>{movie.Title}</h3>
 
-                <div>
-                  <p>
-                    <span>🗓</span>
-                    <span>{movie.Year}</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  <div>
+                    <p>
+                      <span>🗓</span>
+                      <span>{movie.Year}</span>
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          }
         </div>
 
         <div className="box">
