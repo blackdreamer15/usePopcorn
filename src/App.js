d957a0a416d9d6ee55867279423917be73ab2658
Jessy Baki <bakijessy@gmail.com>
2023-08-21 23:56:59 +0000

    Add MovieList component

--- src/App.js
+++ src/App.js
@@ -139,28 +139,32 @@ function ListBox() {
         {isOpen1 ? "+" : "-"}
       </Button>
 
-      {!isOpen1 &&
-        <ul className="list">
-          {movies.map(movie => (
-            <li key={movie.imdbID}>
-              <img
-                src={movie.Poster}
-                alt={`${movie.Title} poster`}
-              />
-
-              <h3>{movie.Title}</h3>
-
-              <div>
-                <p>
-                  <span>🗓</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      }
+      {!isOpen1 && <MovieList />}
     </div>
+  );
+}
+
+function MovieList() {
+  return (
+    <ul className="list">
+      {movies.map(movie => (
+        <li key={movie.imdbID}>
+          <img
+            src={movie.Poster}
+            alt={`${movie.Title} poster`}
+          />
+
+          <h3>{movie.Title}</h3>
+
+          <div>
+            <p>
+              <span>🗓</span>
+              <span>{movie.Year}</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
