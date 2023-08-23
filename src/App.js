c03d3597e6d2bc8366b269de496b0a656e51b600
Jessy Baki <bakijessy@gmail.com>
2023-08-23 00:13:56 +0000

    Add WatchedMovie component and render it

--- src/App.js
+++ src/App.js
@@ -237,32 +237,38 @@ function WatchedMovieList() {
   return (
     <ul className="list">
       {watched.map(movie => (
-        <li key={movie.imdbID}>
-          <img
-            src={movie.Poster}
-            alt={`${movie.Title} poster`}
-          />
-
-          <h3>{movie.Title}</h3>
-
-          <div>
-            <p>
-              <span>⭐</span>
-              <span>{movie.imdbRating}</span>
-            </p>
-
-            <p>
-              <span>🌟</span>
-              <span>{movie.userRating}</span>
-            </p>
-
-            <p>
-              <span>⏳</span>
-              <span>{movie.runtime} mins</span>
-            </p>
-          </div>
-        </li>
+        <WatchedMovie movie={movie} key={movie.imdbID} />
       ))}
     </ul>
+  );
+}
+
+function WatchedMovie({ movie }) {
+  return (
+    <li>
+      <img
+        src={movie.Poster}
+        alt={`${movie.Title} poster`}
+      />
+
+      <h3>{movie.Title}</h3>
+
+      <div>
+        <p>
+          <span>⭐</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} mins</span>
+        </p>
+      </div>
+    </li>
   );
 }
