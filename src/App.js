affb53e8ab5d21d57453a167197fce626e556459
Jessy Baki <bakijessy@gmail.com>
2023-08-24 05:52:42 +0000

    Render the number of movies

--- src/App.js
+++ src/App.js
@@ -103,10 +103,10 @@ function Search() {
   );
 }
 
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
