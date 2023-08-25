4f2a75a99bc1d9aafb6026c4c714e800a7349918
Jessy Baki <bakijessy@gmail.com>
2023-08-25 05:15:14 +0000

    Add minor changes to StarRating

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating
+        color='blue' size={45} className='test'
+        messages={["terible", "bad", "okay", "good", "amazing"]}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars.</p>
+    </>
+  );
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating maxRating={5} color='orangered' size={45} defaultRating={3} />
     <StarRating />
+    < Test />
   </React.StrictMode>
 );
