3f167c400f5e4440f7e5cf317be64119d0a39bb5
Jessy Baki <bakijessy@gmail.com>
2023-08-25 06:15:35 +0000

    Remove code used to test StarRating.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <>
-      <StarRating
-        color='blue' size={45} className='test'
-        messages={["terible", "bad", "okay", "good", "amazing"]}
-        onSetRating={setMovieRating}
-      />
-      <p>This movie was rated {movieRating} stars.</p>
-    </>
-  );
 
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={5} color='orangered' size={45} defaultRating={3} />
-    <StarRating />
-    < Test />
+    <App />
   </React.StrictMode>
 );
