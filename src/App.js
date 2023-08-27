b6d7d33beb5bad8d0206a83411bf71fa91c4ed69
Jessy Baki <bakijessy@gmail.com>
2023-08-27 08:14:33 +0000

    Add the query from the input to the http request

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default function App() {
       async function fetchMovies() {
         try {
           setIsLoading(true);
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s={}`);
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
 
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies.");
