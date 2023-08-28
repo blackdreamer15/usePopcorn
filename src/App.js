f6b87455c1781f4d763cc415745aedcd15833282
Jessy Baki <bakijessy@gmail.com>
2023-08-28 20:11:17 +0000

    Add changes to event handler function for onClick

--- src/App.js
+++ src/App.js
@@ -338,7 +338,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
             </header>
 
             <section>
-              <button className="btn-add" onClick={onAddMovie}>+ Add to list</button>
+              <button className="btn-add" onClick={handleAddMovie}>+ Add to list</button>
 
               <div className="rating">
                 <StarRating maxRating={10} size={24} />
