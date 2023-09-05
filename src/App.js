1b8d987f10f8f9f6d1fc388a438cb422bbcb580e
Jessy Baki <bakijessy@gmail.com>
2023-09-05 06:09:38 +0000

    Extract useEffect inside useMovies.js

--- src/App.js
+++ src/App.js
@@ -51,56 +51,6 @@ export default function App() {
   );
 
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies.");
-
-          const data = await res.json();
-          if (data.Response === "False")
-            throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        }
-        catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        }
-        finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (!query.length) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      handleCloseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      }
-    }, [query]
-  );
-
-
   return (
     <>
       <NavBar>
